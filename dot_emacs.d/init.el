768ede5dd03085269be699694de6f95e908d1643
elderica <1130138+elderica@users.noreply.github.com>
2023-08-11 22:22:58 +0900

    my/favirote-packages

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -8,13 +8,44 @@
         ("org" . "https://orgmode.org/elpa/")
         ("gnu" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
-(package-refresh-contents)
+
+(defvar my/favorite-packages
+  '(
+    elcord
+
+    magit
+    git-gutter
+
+    company
+
+    sly
+    sly-quicklisp
+    sly-named-readtables
+    sly-macrostep
+    sly-asdf
+
+    dockerfile-mode
+
+    flycheck
+    ))
+
+(defun install-my/favorite-packages ()
+  (interactive)
+
+  (package-refresh-contents)
+  (dolist (package my/favorite-packages)
+    (unless (package-installed-p package)
+      (package-install package))))
 
 (load-theme 'misterioso)
 
 (delete-selection-mode t)
 
 (show-paren-mode t)
+
+(which-function-mode t)
+
+(transient-mark-mode t)
 
 (global-auto-revert-mode t)
 
@@ -30,3 +61,6 @@
 (when (package-installed-p 'elcord)
   (require 'elcord)
   (elcord-mode t))
+
+(global-set-key (kbd "M-[") 'switch-to-prev-buffer)
+(global-set-key (kbd "M-]") 'switch-to-next-buffer)
