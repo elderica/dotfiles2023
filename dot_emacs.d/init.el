0a688f4adc1419a14264a83110073a96ecc2b48a
elderica <1130138+elderica@users.noreply.github.com>
2024-11-19 20:16:10 +0900

    emacs: introduce `elpy` package for Python programming

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -321,5 +321,16 @@
 (leaf ocp-indent :ensure t
   :require t)
 
+(leaf elpy :ensure t
+  :init (elpy-enable)
+  :config
+  (remove-hook 'elpy-modules 'elpy-module-flymake)
+  :custom
+  ((elpy-rpc-python-command . "python3")
+   (flycheck-python-flake8-executable . "flake8"))
+  :bind (elpy-mode-map
+         ("C-c C-r f" . elpy-format-code))
+  :hook ((elpy-mode-hook . flycheck-mode)))
+
 (provide 'init)
 ;;; init.el ends here
